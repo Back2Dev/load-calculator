39728ac9d3480b75fecc8d67ccd6db8460181c69
Mike King <mikkelking@hotmail.com>
2024-05-31 10:06:38 +1000

    proper api

--- src/pages/load-calc/components/SubmitButton.jsx
+++ src/pages/load-calc/components/SubmitButton.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Button, Box } from '@mui/material'
+import { Button, Box, Collapse } from '@mui/material'
 import Alert from '@mui/material/Alert'
 import CheckIcon from '@mui/icons-material/Check'
+import ErrorIcon from '@mui/icons-material/WarningAmber'
 import axios from 'axios'
+import cloneDeep from 'lodash/cloneDeep'
 import dbg from 'debug'
 import { useData } from '../DataContext'
 
 const debug = dbg('app:submit')
 
 const SubmitButton = () => {
-  const formData = useData() || {}
+  const theData = useData() || {}
   const [status, setStatus] = React.useState('')
+  const [message, setMessage] = React.useState('')
 
   const submitForm = async () => {
     try {
+      const formData = cloneDeep(theData)
       formData.appliances = Object.entries(formData?.applianceGroups)
         .map(([groupName, appliances]) => {
           return appliances.filter(({ quantity }) => quantity > 0)
@@ -24,35 +28,51 @@ const SubmitButton = () => {
       const body = {
         form: 'dpa-load-calc',
         formData,
+        surveySlug: 'dpa-load-calc-v1',
       }
-      const response = await axios.post('https://dpa.requestcatcher.com/test', body, {
+      const response = await axios.post('http://localhost:2060/forms/submit', body, {
         headers: {
           'Content-Type': 'application/json',
+          Authorization: 'Bearer 95cb-dfc2-64ab-435f-9d14-e5cd-92a9-6d6a-b74e-4130',
+          Accept: 'application/json',
         },
         withCredentials: false,
       })
       if (response.status !== 200)
         return { status: 'failed', message: response.data[0].message }
 
-      if (!response.data.length) setStatus('Failed: Missing data info from response')
+      if (!response.data.length) {
+        setStatus(status)
+        setMessage('Failed: Missing data info from response')
+      }
 
       setStatus('Success')
+      setMessage('')
     } catch (error) {
       console.error(error)
-      setStatus('Failed to send email: ' + error.message)
+      setStatus('Failed')
+      const msg = error.response?.statusText
+        ? `${error.response.status}: ${error.response.statusText}`
+        : error.message
+      setMessage(msg)
     }
   }
 
+  const ok = status.match(/success/i)
   return (
-    <Box my={8}>
+    <Box sx={{ width: '50%', display: 'flex', justifyContent: 'center' }}>
       <Button variant="contained" onClick={submitForm} size="large">
         Submit
       </Button>
-      {status === 'success' && (
-        <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-          Success,
+      <Collapse in={status !== ''}>
+        <Alert
+          icon={ok ? <CheckIcon fontSize="inherit" /> : <ErrorIcon fontSize="inherit" />}
+          severity={ok ? 'success' : 'error'}
+        >
+          {status} {message}
         </Alert>
-      )}
+      </Collapse>
+      )
     </Box>
   )
 }

4739df8919952a1711946b7e78192c93c028b17f
Mike King <mikkelking@hotmail.com>
2024-05-31 10:07:18 +1000

    url

--- src/pages/load-calc/components/SubmitButton.jsx
+++ src/pages/load-calc/components/SubmitButton.jsx
@@ -30,14 +30,18 @@ const SubmitButton = () => {
         formData,
         surveySlug: 'dpa-load-calc-v1',
       }
-      const response = await axios.post('http://localhost:2060/forms/submit', body, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer 95cb-dfc2-64ab-435f-9d14-e5cd-92a9-6d6a-b74e-4130',
-          Accept: 'application/json',
+      const response = await axios.post(
+        'http://forms.dpasolar.com.au/forms/submit',
+        body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer 95cb-dfc2-64ab-435f-9d14-e5cd-92a9-6d6a-b74e-4130',
+            Accept: 'application/json',
+          },
+          withCredentials: false,
         },
-        withCredentials: false,
-      })
+      )
       if (response.status !== 200)
         return { status: 'failed', message: response.data[0].message }
 
